5da324b2cdeb8ee433eb179657ee58776d1ee282
santoshgambhava <santoshgambhava30@gmail.com>
2022-10-11 22:15:09 +0530

    First Commit

---
+++ D_1.py
@@ -0,0 +1,53 @@
+print("welcome to python")
+print("python is best")
+print("welcome to python",end=" ")
+print("python is best")
+a=("santosh")
+b=("Gambhava")
+print(a,end=" ")
+print(b)
+a=10
+print("A:",a)
+print("santosh,",a)
+a=20
+b=30
+c=a+b
+print("addition",c)
+print("*",c)
+print(c)
+a=int(input("enter A:"))
+if a>0:
+    print("A is positive number")
+else:
+    print("A is negative number")
+a=int(input("enter A:"))
+if a>0:
+    print("A is positive number")
+else:
+    print("A is negative number")  
+b=int(input("enter B:"))
+if b/2==0:
+    print("B is even number")
+else:
+    print("B is odd number")    
+
+
+c=int(input("enter A:"))
+d=int(input("enter B:"))
+if c>d:
+    print("A is gtrater number")
+else:
+    print("B is greater number")
+
+rno=int(input("enter roll no:"))
+sname=("entet student name:")
+s1=int(input("enter marns of subject 1:"))
+s2=int(input("enter marns of subject 2:"))
+s3=int(input("enter marns of subject 3:"))
+total=s1+s2+s3
+per=total/3
+print("roll no:",rno)
+print("studen name:",sname)
+print("total:",total)
+print("percentage:",per)
+
